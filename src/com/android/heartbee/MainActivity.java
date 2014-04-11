817e5fa8f4cb73f5e36d786cb38cc1f9a341c95b
Ryan Niemocienski <rniemo@gmail.com>
2014-04-11 19:27:08 -0400

    Added Tab management system

--- src/com/android/heartbee/MainActivity.java
+++ src/com/android/heartbee/MainActivity.java
@@ -1,15 +1,47 @@
 package com.android.heartbee;
 
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	ViewPager viewPager;
+	TabPagerAdapter tabAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		final ActionBar actionBar = getActionBar();
 		setContentView(R.layout.activity_main);
+		tabAdapter = new TabPagerAdapter(this.getSupportFragmentManager());
+		viewPager = (ViewPager) findViewById(R.id.viewPager);
+		viewPager.setAdapter(tabAdapter);
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+	        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+	            tab.select();
+	        }
+
+	        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+	        	
+	            
+	        }
+	        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft){}
+	    };
+
+	    // Add tabs to the action bar
+	    for (int i = 0; i < TabPagerAdapter.NUM_TABS; i++) {
+	    	Tab t = actionBar.newTab()
+                    .setText(TabPagerAdapter.TAB_NAMES[i])
+                    .setTabListener(tabListener);
+	    	tabAdapter.addFragment(new TestFragment(), i);
+	        actionBar.addTab(t);
+	    }
 	}
 
 	@Override
---
+++ src/com/android/heartbee/TestFragment.java
@@ -0,0 +1,10 @@
+package com.android.heartbee;
+
+import android.support.v4.app.Fragment;
+
+
+
+public class TestFragment extends Fragment{
+
+	
+}
