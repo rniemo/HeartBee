7e9c4a509896f8455fa8e76334e9630910f26d4e
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2014-04-11 20:43:16 -0400

    Stash

---
+++ src/com/android/heartbee/StatusFragment.java
@@ -0,0 +1,8 @@
+package com.android.heartbee;
+
+import android.support.v4.app.Fragment;
+
+public final class StatusFragment extends Fragment {
+	public static final String ARG_OBJECT = "object";
+	
+}
